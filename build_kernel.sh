0b4c182ab125eab3191697f3719e1d71a0a3911f
Febby Widyaningsih <47648214+febbywidyaningsih@users.noreply.github.com>
2019-12-11 21:52:01 +0700

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,7 +5,7 @@ export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8
 
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_gtelwifi_usa_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8916_sec_fortunave3g_eur_defconfig msm8916_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=output
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
